2df8ecba7d68c5af38e58477033d46288f5b4894
Kevin Phan <ken.phan@live.ca>
2023-05-19 23:23:08 +0700

    Initial Commit

---
+++ animated-swiftui/animated_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  animated_swiftuiApp.swift
+//  animated-swiftui
+//
+//  Created by phantom on 2023-05-19.
+//
+
+import SwiftUI
+
+@main
+struct animated_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
