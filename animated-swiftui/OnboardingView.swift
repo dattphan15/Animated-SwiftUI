6840235a1f9aea899acb4a7ab67485b461fec24b
Kevin Phan <ken.phan@live.ca>
2023-05-21 22:09:05 +0700

    - Add Start button to main screen

--- animated-swiftui/OnboardingView.swift
+++ animated-swiftui/OnboardingView.swift
@@ -10,14 +10,19 @@ import RiveRuntime
 
 struct OnboardingView: View {
     var body: some View {
-        RiveViewModel(fileName: "shapes").view()
-            .ignoresSafeArea()
-            .blur(radius: 30)
-            .background(
-                Image("Spline")
-                    .blur(radius: 50)
-                    .offset(x: 200, y: 100)
-                )
+        ZStack {
+            RiveViewModel(fileName: "shapes").view()
+                .ignoresSafeArea()
+                .blur(radius: 30)
+                .background(
+                    Image("Spline")
+                        .blur(radius: 50)
+                        .offset(x: 200, y: 100)
+            )
+            
+            RiveViewModel(fileName: "button").view()
+                .frame(width: 236, height: 64)
+        }
     }
 }
 
