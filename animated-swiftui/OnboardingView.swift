18d91f7eb219d48a3f49544f1d4336bad39a1634
Kevin Phan <ken.phan@live.ca>
2023-05-22 02:33:48 +0700

    - Add button icon and animation, Set view variables for readability

--- animated-swiftui/OnboardingView.swift
+++ animated-swiftui/OnboardingView.swift
@@ -9,20 +9,40 @@ import SwiftUI
 import RiveRuntime
 
 struct OnboardingView: View {
+    let button = RiveViewModel(fileName: "button")
+    
     var body: some View {
         ZStack {
-            RiveViewModel(fileName: "shapes").view()
-                .ignoresSafeArea()
-                .blur(radius: 30)
-                .background(
-                    Image("Spline")
-                        .blur(radius: 50)
-                        .offset(x: 200, y: 100)
-            )
+            background
             
-            RiveViewModel(fileName: "button").view()
+            button.view()
                 .frame(width: 236, height: 64)
+                .overlay(
+                    Label("Start Demo", systemImage: "arrow.forward")
+                .offset(x: 4, y: 4)
+                .font(.headline)
+                )
+                .background(
+                    Color.black
+                        .cornerRadius(30)
+                        .blur(radius: 30)
+                        .opacity(0.3)
+                        .offset(y: 10)
+                )
+                .onTapGesture {
+                    button.play(animationName: "active")
+                }
         }
+    }
+    
+    var background: some View {             RiveViewModel(fileName: "shapes").view()
+            .ignoresSafeArea()
+            .blur(radius: 30)
+            .background(
+                Image("Spline")
+                    .blur(radius: 50)
+                    .offset(x: 200, y: 100)
+            )
     }
 }
 
