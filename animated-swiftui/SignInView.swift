3a2d253c10ac910876c00a4c858b1033878c09fd
Kevin Phan <ken.phan@live.ca>
2023-05-31 23:59:20 +0700

    .

--- animated-swiftui/SignInView.swift
+++ animated-swiftui/SignInView.swift
@@ -24,7 +24,7 @@ struct SignInView: View {
                 .foregroundColor(.secondary)
                 TextField("", text: $email)
                     .customTextField()
-            }
+            
             
             Divider()
             
