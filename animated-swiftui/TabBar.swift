4c1117a3c60d1b6bcac9b887b72dc473fee79917
Kevin Phan <ken.phan@live.ca>
2023-06-14 16:15:59 -0700

    Style TabBar background

--- animated-swiftui/TabBar.swift
+++ animated-swiftui/TabBar.swift
@@ -12,19 +12,29 @@ struct TabBar: View {
     let icon = RiveViewModel(fileName: "icons", stateMachineName: "CHAT_Interactivity", artboardName: "CHAT")
     
     var body: some View {
-        HStack {
-            ForEach(tabItems) { item in
-                Button {
-                    try? item.icon.setInput("active", value: true)
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                        try? item.icon.setInput("active", value: false)
-                    }
-                } label: {
-                    item.icon.view()
+        
+        VStack {
+            Spacer()
+            HStack {
+                ForEach(tabItems) { item in
+                    Button {
+                        try? item.icon.setInput("active", value: true)
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                            try? item.icon.setInput("active", value: false)
+                        }
+                    } label: {
+                        item.icon.view()
+                            .frame(height: 36)
+                }
+                }
             }
-            }
+            .padding(12)
+            .background(Color("Background 2").opacity(0.8))
+            .background(.ultraThinMaterial)
+            .mask(RoundedRectangle(cornerRadius: 24, style: .continuous))
+            .shadow(color: Color("Background 2").opacity(0.3), radius: 20, x: 0, y: 20)
+            .padding(.horizontal, 24)
         }
-        .background(Color("Background 2").opacity(0.8))
     }
 }
 
