0899b4c88f986018dc888735c75cfda07911ecae
Kevin Phan <ken.phan@live.ca>
2023-06-15 23:26:25 -0700

    Refactor TarBar

--- animated-swiftui/TabBar.swift
+++ animated-swiftui/TabBar.swift
@@ -12,28 +12,36 @@ struct TabBar: View {
     let icon = RiveViewModel(fileName: "icons", stateMachineName: "CHAT_Interactivity", artboardName: "CHAT")
     
     var body: some View {
-        
         VStack {
             Spacer()
             HStack {
-                ForEach(tabItems) { item in
-                    Button {
-                        try? item.icon.setInput("active", value: true)
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                            try? item.icon.setInput("active", value: false)
-                        }
-                    } label: {
-                        item.icon.view()
-                            .frame(height: 36)
-                }
-                }
+                content
             }
             .padding(12)
             .background(Color("Background 2").opacity(0.8))
             .background(.ultraThinMaterial)
             .mask(RoundedRectangle(cornerRadius: 24, style: .continuous))
             .shadow(color: Color("Background 2").opacity(0.3), radius: 20, x: 0, y: 20)
+            .overlay(
+                RoundedRectangle(cornerRadius: 24, style:
+                        .continuous)
+                        .stroke(.linearGradient(colors:[.white.opacity(0.5), .white.opacity(0)], startPoint: .topLeading, endPoint: .bottomTrailing))
+                )
             .padding(.horizontal, 24)
+        }
+    }
+    
+    var content: some View {
+        ForEach(tabItems) { item in
+            Button {
+                try? item.icon.setInput("active", value: true)
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    try? item.icon.setInput("active", value: false)
+                }
+            } label: {
+                item.icon.view()
+                    .frame(height: 36)
+            }
         }
     }
 }
